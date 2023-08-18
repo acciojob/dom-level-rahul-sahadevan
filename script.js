804fedccb6eceae863484075607c58f5f5399e8a
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-18 10:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let n = 1;
+
+const ele = document.getElementsByTagName("li");
+console.log(ele.length);
+
+for(let i =0;i<ele.length;i++){
+	let e = ele[i];
+	if(e.id === "level"){
+		break;
+	}
+	n++;
+}
+console.log(alert(n));
